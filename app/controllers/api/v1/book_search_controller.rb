3b7dcbf937182726e6708081732aeaa6a6a17b9b
Brett Jones <bhjones4545@gmail.com>
2021-09-27 10:58:57 -0600

    Change syntax on params

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-    books = BookSearchFacade.get_books(params[:location], params[:quantity])
-    if params[:quantity].to_i <= 0
+    books = BookSearchFacade.get_books(params[:location], params[:limit])
+    if params[:limit].to_i <= 0
       render json: { error: "Quantity must be greater than 0" }, status: 400
     else
       render json: BookSearchSerializer.new(books), status: 200
