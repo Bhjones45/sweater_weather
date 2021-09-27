2294cec8ce909b7b56260d739ff2ee4561896081
Brett Jones <bhjones4545@gmail.com>
2021-09-27 10:31:08 -0600

    Fix syntax missing end

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
+    books = BookSearchFacade.get_books(params[:location], params[:quantity])
+    if params[:quantity].to_i <= 0
+      render json: { error: "Quantity must be greater than 0" }, status: 400
+    else
+      render json: BookSearchSerializer.new(books), status: 200
   end
+end
 end
