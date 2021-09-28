757a1258c3ab26167be84ceb8808883b6d57c8a0
Brett Jones <bhjones4545@gmail.com>
2021-09-28 14:02:48 -0600

    Create poro test for road trip

---
+++ spec/poros/road_trip_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+RSpec.describe RoadTripDetails do
+  describe 'class methods' do
+    describe '::initialize' do
+      it 'create road trip object', :vcr do
+        forecast = WeatherFacade.forecast("estes park,co")
+        time = Time.now + 5605
+
+        info = {
+          :start_city: "denver,co",
+          :end_city: "estes park,co",
+          :travel_time: ["01", "22", "49"],
+          :weather: forecast,
+          :time: time
+        }
+
+        roadtrip_info = RoadTripDetails.new(details)
+
+        expect(roadtrip_info.id).to eq ("null")
+        expect(roadtrip_info.start_city).to eq("denver,co")
+        expect(roadtrip_info.end_city).to eq("estes park,co")
+        expect(roadtrip_info.travel_time).to eq('01 hours, 22 minuets')
+        expect(roadtrip_info.waether_at_eta[:temperature]).to eq('80 F')
+        expect(roadtrip_info.waether_at_eta[:conditions]).to eq('x')
+      end
+    end
+  end
+end
