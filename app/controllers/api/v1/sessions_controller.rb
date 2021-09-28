5970033311d2b7cf7540b201d847cc7377c7d5ed
Brett Jones <bhjones4545@gmail.com>
2021-09-28 12:43:00 -0600

    WIP

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::SessionsController < ApplicationController
   def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(user_params[:password])
+      render json: UsersSerializer.new(user), status: 201
   end
+
+
+  private
+    def user_params
+      params.permit(:email, :password)
+    end
 end
