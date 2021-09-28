b36dd647252e52b300a814aebd008cb7e14eaf1c
Brett Jones <bhjones4545@gmail.com>
2021-09-28 13:48:23 -0600

    Added downcase for edge case

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -4,7 +4,7 @@ class MapQuestService
   end
 
   def self.find_route(start_city, end_city)
-    new.request_api("/directions/v2/route?from=#{start_city}&to=#{end_city}")
+    new.request_api("/directions/v2/route?from=#{start_city.downcase}&to=#{end_city.downcase}")
   end
 
   def request_api(path)
