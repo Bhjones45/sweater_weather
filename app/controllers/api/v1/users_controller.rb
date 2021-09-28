c1577e7f950140123339195bdf2544be65962f0c
Brett Jones <bhjones4545@gmail.com>
2021-09-28 11:41:22 -0600

    WIP

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
   end
+
+
+  private
+    def user_params
+      params.permit(:email, :password, :password_confirmation)
+    end
 end
