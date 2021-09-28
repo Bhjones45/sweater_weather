9b734ffe192d3948afe02df2b250ee6d9ae2f1db
Brett Jones <bhjones4545@gmail.com>
2021-09-28 14:28:07 -0600

    wip

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
+    user = User.find_by(api_key: road_trip_params[:api_key])
+    if user
+      trip = RoadTripFacade.road_trip(road_trip_params[:origin], road_trip_params[:destination])
+      render json: RoadTripSerializer.new(trip), status: 200
+    else
+      x
+    end
   end
+
+  private
+    def road_trip_params
+      params.permit(:origin, :destination, :api_key)
+    end
 end
